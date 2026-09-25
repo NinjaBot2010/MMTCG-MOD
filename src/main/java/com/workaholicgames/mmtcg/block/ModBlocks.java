4a9f83d927e5dab4134c4b593f9919aa71642f99
NinjaBot2010 <jonathan.emmons@outlook.com>
2026-09-25 13:30:23 -0700

    Updated maximite ore to sound like deepslate

--- src/main/java/com/workaholicgames/mmtcg/block/ModBlocks.java
+++ src/main/java/com/workaholicgames/mmtcg/block/ModBlocks.java
@@ -21,7 +21,7 @@ public class ModBlocks {
 
     public static final DeferredBlock<Block> MAXIMITE_ORE = registerBlock("maximite_ore",
             () -> new DropExperienceBlock(UniformInt.of(2, 4), BlockBehaviour.Properties.of()
-                    .strength(4f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
+                    .strength(4f).requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE)));
 
     private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
         DeferredBlock<T> toReturn = BLOCKS.register(name, block);
